6b6b8165dca0d07d9d3da9484318f80dfa32c5ae
anram88 <aramirez@kenzanmedia.com>
2016-01-07 18:18:23 -0600

    Initialized ratings data client

---
+++ src/main/java/com/kenzan/msl/ratings/client/dao/UserRatingsDao.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2015, Charter Communications, All rights reserved.
+ */
+package com.kenzan.msl.ratings.client.dao;
+
+import com.datastax.driver.mapping.annotations.Column;
+import com.datastax.driver.mapping.annotations.PartitionKey;
+import com.datastax.driver.mapping.annotations.Table;
+import java.util.UUID;
+import com.kenzan.msl.common.dao.AbstractDao;
+
+/**
+ * @author billschwanitz
+ */
+@Table(name = "user_ratings")
+public class UserRatingsDao extends AbstractDao {
+    @PartitionKey
+    @Column(name = "user_id")
+    private UUID userId;
+    @Column(name = "content_type")
+    private String contentType;
+    @Column(name = "content_uuid")
+    private UUID contentUuid;
+    @Column(name = "rating")
+    private Integer rating;
+
+    /**
+     * @return the userId
+     */
+    public UUID getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId the userId to set
+     */
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @return the contentType
+     */
+    public String getContentType() {
+        return contentType;
+    }
+
+    /**
+     * @param contentType the contentType to set
+     */
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    /**
+     * @return the contentUuid
+     */
+    public UUID getContentUuid() {
+        return contentUuid;
+    }
+
+    /**
+     * @param contentUuid the contentUuid to set
+     */
+    public void setContentUuid(UUID contentUuid) {
+        this.contentUuid = contentUuid;
+    }
+
+    /**
+     * @return the rating
+     */
+    public Integer getRating() {
+        return rating;
+    }
+
+    /**
+     * @param rating the rating to set
+     */
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+}
---
+++ src/main/java/com/kenzan/msl/ratings/client/services/RatingsService.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015, Charter Communications, All rights reserved.
+ */
+package com.kenzan.msl.ratings.client.services;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.mapping.Result;
+import com.google.common.base.Optional;
+import com.kenzan.msl.ratings.client.dao.AverageRatingsDao;
+import com.kenzan.msl.ratings.client.dao.UserRatingsDao;
+import rx.Observable;
+
+import java.util.UUID;
+
+public interface RatingsService {
+
+    // ================================================================================================================
+    // AVERAGE RATINGS
+    // ================================================================================================================
+
+    Observable<Void> addOrUpdateAverageRating(AverageRatingsDao averageRatingDao);
+
+    Observable<AverageRatingsDao> getAverageRating(UUID contentId, String contentType);
+
+    Observable<Void> deleteAverageRating(UUID contentId, String contentType);
+
+    // ================================================================================================================
+    // USER RATINGS
+    // ================================================================================================================
+
+    Observable<Void> addOrUpdateUserRatings(UserRatingsDao userRatingsDao);
+
+    Observable<UserRatingsDao> getUserRating(UUID userUuid, String contentType, UUID contentUuid);
+
+    Observable<ResultSet> getUserRatings(UUID userUuid, Optional<String> contentType, Optional<Integer> limit);
+
+    Observable<Result<UserRatingsDao>> mapUserRatings(Observable<ResultSet> object);
+
+    Observable<Void> deleteUserRatings(UUID userUuid, String contentType, UUID contentUuid);
+}
