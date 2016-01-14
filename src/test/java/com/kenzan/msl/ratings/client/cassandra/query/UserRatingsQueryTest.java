23bdf9df1a43e3f11f775bdbb7fa02134a969d92
Brayan Z <brayan@pernix-solutions.com>
2016-01-14 09:31:00 -0600

    Add unit tests

--- src/test/java/com/kenzan/msl/ratings/client/cassandra/query/UserRatingsQueryTest.java
+++ src/test/java/com/kenzan/msl/ratings/client/cassandra/query/UserRatingsQueryTest.java
@@ -1,97 +1,165 @@
 package com.kenzan.msl.ratings.client.cassandra.query;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.mapping.Mapper;
+import com.datastax.driver.mapping.Result;
 import com.google.common.base.Optional;
 
+import com.kenzan.msl.ratings.client.dao.AverageRatingsDao;
+import com.kenzan.msl.ratings.client.dao.UserRatingsDao;
+import org.easymock.EasyMock;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import com.datastax.driver.mapping.MappingManager;
 import com.kenzan.msl.ratings.client.TestConstants;
 import com.kenzan.msl.ratings.client.cassandra.QueryAccessor;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ MappingManager.class })
 public class UserRatingsQueryTest {
+    @Mock
+    private QueryAccessor queryAccessor;
+
     private TestConstants tc = TestConstants.getInstance();
 
-    @Mock
-    private QueryAccessor queryAccessor = Mockito.mock(QueryAccessor.class);
-    @Mock
-    private MappingManager mappingManager;
+    private MappingManager manager;
+    private Mapper mapper;
+    private Result result;
+    private ResultSet resultSet;
+
+    @Before
+    public void init()
+        throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        resultSet = EasyMock.createMock(ResultSet.class);
+        manager = PowerMockito.mock(MappingManager.class);
+        mapper = PowerMockito.mock(Mapper.class);
+        result = PowerMockito.mock(Result.class);
+    }
 
     @Test
     public void testAdd() {
-        // TODO: remove try/catch & mock method throwing exception
-        try {
-            UserRatingsQuery.add(queryAccessor, mappingManager, tc.USER_RATINGS_DAO);
-        }
-        catch ( Exception expected ) {
-            verify(queryAccessor, atLeastOnce()).setUserRating(tc.USER_ID, tc.ALBUM_ID, tc.ALBUM_CONTENT_TYPE,
-                                                               tc.RATING);
-        }
+        mockGetMethod();
+        tc.USER_RATINGS_DAO.setContentType("Album");
+        UserRatingsQuery.add(queryAccessor, manager, tc.USER_RATINGS_DAO);
+        verify(queryAccessor, atLeastOnce()).setUserRating(tc.USER_RATINGS_DAO.getUserId(),
+                                                           tc.USER_RATINGS_DAO.getContentUuid(),
+                                                           tc.USER_RATINGS_DAO.getContentType(),
+                                                           tc.USER_RATINGS_DAO.getRating());
     }
 
     @Test(expected = RuntimeException.class)
-    public void testAddThrowsException() {
-        UserRatingsQuery.add(queryAccessor, mappingManager, null);
+    public void testUnableToAdd() {
+        mockNullGetMethod();
+        UserRatingsQuery.add(queryAccessor, manager, tc.USER_RATINGS_DAO);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testInvalidContentTypeExceptionAdd() {
+        tc.USER_RATINGS_DAO.setContentType("INVALID_CONTENT_TYPE");
+        UserRatingsQuery.add(queryAccessor, manager, tc.USER_RATINGS_DAO);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testUnableToAddException() {
+        mockNullGetMethod();
+        UserRatingsQuery.add(queryAccessor, manager, tc.USER_RATINGS_DAO);
     }
 
     @Test
     public void testGetRating() {
-        // TODO: remove try/catch & mock method throwing exception
-        try {
-            UserRatingsQuery.getRating(queryAccessor, mappingManager, tc.USER_ID, tc.ALBUM_ID, tc.ALBUM_CONTENT_TYPE);
-        }
-        catch ( Exception expected ) {
-            verify(queryAccessor, atLeastOnce()).getUserRating(tc.USER_ID, tc.ALBUM_ID, tc.ALBUM_CONTENT_TYPE);
-        }
-    }
-
-    @Test
-    public void testGetRatings() {
-        UserRatingsQuery.getRatings(queryAccessor, tc.USER_ID, Optional.absent(), Optional.absent());
-        verify(queryAccessor, atLeastOnce()).getUserRatings(tc.USER_ID);
+        mockGetMethod();
+        UserRatingsDao results = UserRatingsQuery.getRating(queryAccessor, manager, tc.USER_ID, tc.ALBUM_ID,
+                                                            tc.ALBUM_CONTENT_TYPE);
+        assertNotNull(results);
+        assertEquals(results, tc.USER_RATINGS_DAO);
     }
 
     @Test(expected = RuntimeException.class)
-    public void testGetRatingThrowsException() {
-        UserRatingsQuery.getRating(queryAccessor, mappingManager, tc.USER_ID, tc.ALBUM_ID, null);
+    public void testGetInvalidContentType() {
+        UserRatingsQuery.getRating(queryAccessor, manager, tc.USER_ID, tc.ALBUM_ID, "INVALID_CONTENT_TYPE");
     }
 
     @Test
-    public void testGetRatingsWithTypeAndLimit() {
-        UserRatingsQuery.getRatings(queryAccessor, tc.USER_ID, Optional.of(tc.ALBUM_CONTENT_TYPE),
-                                    Optional.of(tc.LIMIT));
-        verify(queryAccessor, atLeastOnce())
-            .getUserRatingsWithTypeAndLimit(tc.USER_ID, tc.ALBUM_CONTENT_TYPE, tc.LIMIT);
-    }
-
-    @Test
-    public void testGetRatingsWithType() {
-        UserRatingsQuery.getRatings(queryAccessor, tc.USER_ID, Optional.of(tc.ALBUM_CONTENT_TYPE), Optional.absent());
-        verify(queryAccessor, atLeastOnce()).getUserRatingsWithType(tc.USER_ID, tc.ALBUM_CONTENT_TYPE);
-    }
-
-    @Test
-    public void testGetRatingsWithLimit() {
-        UserRatingsQuery.getRatings(queryAccessor, tc.USER_ID, Optional.absent(), Optional.of(tc.LIMIT));
-        verify(queryAccessor, atLeastOnce()).getUserRatingsWithLimit(tc.USER_ID, tc.LIMIT);
-    }
-
-    @Test
-    public void testRemove() {
-        // TODO: remove try/catch & mock method throwing exception
-        try {
-            UserRatingsQuery.remove(queryAccessor, mappingManager, tc.USER_ID, tc.ALBUM_ID, tc.ALBUM_CONTENT_TYPE);
-        }
-        catch ( Exception expected ) {
-            verify(queryAccessor, atLeastOnce()).deleteUserRating(tc.USER_ID, tc.ALBUM_ID, tc.ALBUM_CONTENT_TYPE);
-        }
+    public void testDelete() {
+        mockNullGetMethod();
+        UserRatingsQuery.remove(queryAccessor, manager, tc.USER_ID, tc.ALBUM_ID, tc.ALBUM_CONTENT_TYPE);
+        verify(queryAccessor, atLeastOnce()).deleteUserRating(tc.USER_ID, tc.ALBUM_ID, tc.ALBUM_CONTENT_TYPE);
     }
 
     @Test(expected = RuntimeException.class)
-    public void testRemoveThrowsException() {
-        UserRatingsQuery.remove(queryAccessor, mappingManager, tc.USER_ID, tc.ALBUM_ID, null);
+    public void testUnableToDelete() {
+        mockGetMethod();
+        UserRatingsQuery.remove(queryAccessor, manager, tc.USER_ID, tc.ALBUM_ID, tc.ALBUM_CONTENT_TYPE);
+        verify(queryAccessor, atLeastOnce()).deleteUserRating(tc.USER_ID, tc.ALBUM_ID, tc.ALBUM_CONTENT_TYPE);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testDeleteInvalidContentException() {
+        UserRatingsQuery.remove(queryAccessor, manager, tc.USER_ID, tc.ALBUM_ID, "INVALID_CONTENT_TYPE");
+    }
+
+    @Test
+    public void testGetRatingsWithLimitAndContentType() {
+        Mockito.when(queryAccessor.getUserRatingsWithTypeAndLimit(tc.USER_ID, tc.ALBUM_CONTENT_TYPE, tc.LIMIT))
+            .thenReturn(resultSet);
+        ResultSet results = UserRatingsQuery.getRatings(queryAccessor, tc.USER_ID, Optional.of(tc.ALBUM_CONTENT_TYPE),
+                                                        Optional.of(tc.LIMIT));
+        assertEquals(results, resultSet);
+    }
+
+    @Test
+    public void testGetRatingsWithContentType() {
+        Mockito.when(queryAccessor.getUserRatingsWithType(tc.USER_ID, tc.ALBUM_CONTENT_TYPE)).thenReturn(resultSet);
+        ResultSet results = UserRatingsQuery.getRatings(queryAccessor, tc.USER_ID, Optional.of(tc.ALBUM_CONTENT_TYPE),
+                                                        Optional.absent());
+        assertEquals(results, resultSet);
+    }
+
+    @Test
+    public void testGetUserRatingsWithLimit() {
+        Mockito.when(queryAccessor.getUserRatingsWithLimit(tc.USER_ID, tc.LIMIT)).thenReturn(resultSet);
+        ResultSet results = UserRatingsQuery.getRatings(queryAccessor, tc.USER_ID, Optional.absent(),
+                                                        Optional.of(tc.LIMIT));
+        assertEquals(results, resultSet);
+    }
+
+    @Test
+    public void testGetUserRatings() {
+        Mockito.when(queryAccessor.getUserRatings(tc.USER_ID)).thenReturn(resultSet);
+        ResultSet results = UserRatingsQuery
+            .getRatings(queryAccessor, tc.USER_ID, Optional.absent(), Optional.absent());
+        assertEquals(results, resultSet);
+    }
+
+    /* ******************************************************* */
+
+    private void mockGetMethod() {
+        Mockito.when(queryAccessor.getUserRating(Mockito.anyObject(), Mockito.anyObject(), Mockito.anyObject()))
+            .thenReturn(resultSet);
+        PowerMockito.when(manager.mapper(UserRatingsDao.class)).thenReturn(mapper);
+        PowerMockito.when(mapper.map(resultSet)).thenReturn(result);
+        PowerMockito.when(result.one()).thenReturn(tc.USER_RATINGS_DAO);
+    }
+
+    private void mockNullGetMethod() {
+        Mockito.when(queryAccessor.getUserRating(Mockito.anyObject(), Mockito.anyObject(), Mockito.anyObject()))
+            .thenReturn(resultSet);
+        PowerMockito.when(manager.mapper(UserRatingsDao.class)).thenReturn(mapper);
+        PowerMockito.when(mapper.map(resultSet)).thenReturn(result);
+        PowerMockito.when(result.one()).thenReturn(null);
     }
 }
