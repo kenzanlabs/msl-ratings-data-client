364058e31700f2ed7d4ff82ce06562f9e55b929b
anram88 <aramirez@kenzanmedia.com>
2016-03-01 19:01:48 -0600

    MSL-376: implement google style guide

--- src/test/java/com/kenzan/msl/ratings/client/TestConstants.java
+++ src/test/java/com/kenzan/msl/ratings/client/TestConstants.java
@@ -7,35 +7,35 @@ import com.kenzan.msl.ratings.client.dto.UserRatingsDto;
 
 public class TestConstants {
 
-    private static TestConstants instance = null;
+  private static TestConstants instance = null;
 
-    public UUID ALBUM_ID = UUID.fromString("2883607a-176d-4729-a20b-ec441c285afb");
-    public UUID USER_ID = UUID.fromString("2883607a-176d-4729-a20b-ec441c285afb");
-    public String ALBUM_CONTENT_TYPE = "Album";
-    public int RATING = 2;
-    public Long NUM_RATING = Long.valueOf("4");
-    public Long SUM_RATING = Long.valueOf("3");
-    public int LIMIT = 12;
+  public UUID ALBUM_ID = UUID.fromString("2883607a-176d-4729-a20b-ec441c285afb");
+  public UUID USER_ID = UUID.fromString("2883607a-176d-4729-a20b-ec441c285afb");
+  public String ALBUM_CONTENT_TYPE = "Album";
+  public int RATING = 2;
+  public Long NUM_RATING = Long.valueOf("4");
+  public Long SUM_RATING = Long.valueOf("3");
+  public int LIMIT = 12;
 
-    public AverageRatingsDto AVERAGE_RATINGS_DTO = new AverageRatingsDto();
-    public UserRatingsDto USER_RATINGS_DTO = new UserRatingsDto();
+  public AverageRatingsDto AVERAGE_RATINGS_DTO = new AverageRatingsDto();
+  public UserRatingsDto USER_RATINGS_DTO = new UserRatingsDto();
 
-    private TestConstants() {
-        AVERAGE_RATINGS_DTO.setContentId(ALBUM_ID);
-        AVERAGE_RATINGS_DTO.setContentType(ALBUM_CONTENT_TYPE);
-        AVERAGE_RATINGS_DTO.setNumRating(NUM_RATING);
-        AVERAGE_RATINGS_DTO.setSumRating(SUM_RATING);
+  private TestConstants() {
+    AVERAGE_RATINGS_DTO.setContentId(ALBUM_ID);
+    AVERAGE_RATINGS_DTO.setContentType(ALBUM_CONTENT_TYPE);
+    AVERAGE_RATINGS_DTO.setNumRating(NUM_RATING);
+    AVERAGE_RATINGS_DTO.setSumRating(SUM_RATING);
 
-        USER_RATINGS_DTO.setContentType(ALBUM_CONTENT_TYPE);
-        USER_RATINGS_DTO.setContentUuid(ALBUM_ID);
-        USER_RATINGS_DTO.setRating(RATING);
-        USER_RATINGS_DTO.setUserId(USER_ID);
+    USER_RATINGS_DTO.setContentType(ALBUM_CONTENT_TYPE);
+    USER_RATINGS_DTO.setContentUuid(ALBUM_ID);
+    USER_RATINGS_DTO.setRating(RATING);
+    USER_RATINGS_DTO.setUserId(USER_ID);
+  }
+
+  public static TestConstants getInstance() {
+    if (instance == null) {
+      instance = new TestConstants();
     }
-
-    public static TestConstants getInstance() {
-        if ( instance == null ) {
-            instance = new TestConstants();
-        }
-        return instance;
-    }
+    return instance;
+  }
 }
